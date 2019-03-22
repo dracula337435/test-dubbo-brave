fadb519c84f970d17f90cbb339a35b2082a509f6
dk <dracula337435@163.com>
2019-03-22 11:19:04 +0800

    补提交遗漏文件

---
+++ interface/src/main/java/io/dracula/test/dubbo/brave/InterfaceE.java
@@ -0,0 +1,15 @@
+package io.dracula.test.dubbo.brave;
+
+/**
+ * @author dk
+ */
+public interface InterfaceE {
+
+    /**
+     *
+     * @param name
+     * @return
+     */
+    String toE(String name);
+
+}
