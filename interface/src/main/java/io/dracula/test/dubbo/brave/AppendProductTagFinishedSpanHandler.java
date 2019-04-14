e4f840557e37e5615314f5343d2533422df0336c
dracula337435 <dracula337435@163.com>
2019-04-14 13:17:02 +0800

    使用FinishedSpanHandler，在span被送到zipkin之前做些改动。用tag打上产品信息

---
+++ interface/src/main/java/io/dracula/test/dubbo/brave/AppendProductTagFinishedSpanHandler.java
@@ -0,0 +1,14 @@
+package io.dracula.test.dubbo.brave;
+
+import brave.handler.FinishedSpanHandler;
+import brave.handler.MutableSpan;
+import brave.propagation.TraceContext;
+
+public class AppendProductTagFinishedSpanHandler extends FinishedSpanHandler {
+    @Override
+    public boolean handle(TraceContext context, MutableSpan span) {
+        String localServiceName = span.localServiceName();
+        span.tag("LocalProduct", localServiceName+"-product");
+        return true;
+    }
+}
