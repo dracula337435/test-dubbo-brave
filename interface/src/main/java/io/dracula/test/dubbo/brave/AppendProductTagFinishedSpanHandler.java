d156d2221069f71c05d93c4e47326b9665376683
dracula337435 <dracula337435@163.com>
2019-04-16 23:36:07 +0800

    细节修改，增加空行

--- interface/src/main/java/io/dracula/test/dubbo/brave/AppendProductTagFinishedSpanHandler.java
+++ interface/src/main/java/io/dracula/test/dubbo/brave/AppendProductTagFinishedSpanHandler.java
@@ -5,10 +5,12 @@ import brave.handler.MutableSpan;
 import brave.propagation.TraceContext;
 
 public class AppendProductTagFinishedSpanHandler extends FinishedSpanHandler {
+
     @Override
     public boolean handle(TraceContext context, MutableSpan span) {
         String localServiceName = span.localServiceName();
         span.tag("LocalProduct", localServiceName+"-product");
         return true;
     }
+
 }
