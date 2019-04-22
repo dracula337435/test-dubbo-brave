bb58d2a4b46ffd4ef0c283499f3d65b88ced5458
dk <dracula337435@163.com>
2019-04-22 13:45:34 +0800

    初步试验attachments的传递，自动在整个链条中传递。但是只在第一个调用中传递，在a中set好，可在a-e-b-c自动传递，b-d传递不了

--- interface/src/main/java/io/dracula/test/dubbo/brave/ProductFilter.java
+++ interface/src/main/java/io/dracula/test/dubbo/brave/ProductFilter.java
@@ -11,9 +11,37 @@ public class ProductFilter implements Filter {
 
     private static Logger logger = LoggerFactory.getLogger(ProductFilter.class);
 
+    private String product = UNKNOWN_PRODUCT_NAME;
+
+    public static final String UNKNOWN_PRODUCT_NAME = "unknown";
+
+    public static final String PRODUCT_ATTACHMENTS_KEY = "product";
+
     @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
         logger.info("in filter for product");
+        RpcContext rpcContext = RpcContext.getContext();
+        boolean isProviderSide = rpcContext.isProviderSide();
+        // 如果是提供者
+        if(isProviderSide){
+            String remoteProduct = rpcContext.getAttachment(PRODUCT_ATTACHMENTS_KEY);
+            logger.info("remoteProduct"+remoteProduct);
+            // 如果本product为null
+            // 如果传来的product为null
+            // 如果不一致
+            if(!product.equals(remoteProduct)){
+
+            }
+        }
+        // 如果是消费者
+        else{
+            if(invoker.getInterface().getName().contains("a")){
+                logger.info("放入attachment");
+                rpcContext.setAttachment(PRODUCT_ATTACHMENTS_KEY, "product-a");
+            }else{
+                logger.info("不放入attachment");
+            }
+        }
         return invoker.invoke(invocation);
     }
 
